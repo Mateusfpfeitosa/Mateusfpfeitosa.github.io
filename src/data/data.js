e71ea36afb1a7473ca72ad7fba20bbeaf23174f5
Mateusfpfeitosa <mateus.feitosa.felipe@gmail.com>
2023-03-27 10:36:12 -0300

    Inclusão de projetos 1/3

--- src/data/data.js
+++ src/data/data.js
@@ -1,34 +1,42 @@
-import WorkIm from '../assets/projects/workImg.jpeg'
-import RealEst from '../assets/projects/realestate.jpg'
-import Portifolio from '../assets/projects/Portifolio2.png'
-
+import Portifolio from '../assets/projects/Portifolio___.png'
+import Chale from '../assets/projects/Chale-Hotel.png'
+import Museu from '../assets/projects/Museu-Nacional.png'
+import Tecno from '../assets/projects/Tecno-Blog.png'
+import Appimg from '../assets/projects/Img1.jpeg'
 
 export const data=[
     {
         id:1,
         name:"React JS Aplicação",
         image:Portifolio,
-        github:"",
+        github:"https://github.com/Mateusfpfeitosa/Portifolio",
         live:"",
     },
     {
         id:2,
-        name:"React JS Application",
-        image:RealEst,
+        name:"Site Desenvolvido com HTML CSS JavaScript",
+        image:Chale,
         github:"",
         live:"",
     },
     {
         id:3,
         name:"React JS Application",
-        image:WorkIm,
+        image:Museu,
         github:"",
         live:"",
     },
     {
         id:4,
         name:"React JS Application",
-        image:RealEst,
+        image:Tecno,
+        github:"",
+        live:"",
+    },
+    {
+        id:5,
+        name:"React JS Application",
+        image:Appimg,
         github:"",
         live:"",
     },
